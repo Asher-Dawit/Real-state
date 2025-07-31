2665021ed93ecde6dc3c77a88c07d44dc24e672c
asher <asherasherdawit@gmail.com>
2025-07-31 01:31:31 -0400

    complete show user listings functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -189,26 +189,31 @@ try {
       </p>
       <button onClick={handleShowListings} className="text-green-700 w-full">Show Listings</button>
       <p className="text-red-700 mt-5">{showListingsError ? 'Error showing listings' : ''}</p>
-      {userListings && userListings.length > 0 && userListings.map((listing) => 
-        <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
-          <Link to={`/listing/${listing._id}`}>
-          <img src={listing.imageUrls[0]} alt="listing cover" 
-          className="h-16 w-16 object-contain"
-          />
-          </Link> 
-          <Link className="text-slate-700 semibold hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
-          <p>{listing.name}</p>
-          </Link>
-          
-          <div className="flex flex-col item-center">
- 
-         <button className="text-red-700 uppercase">Delete</button>
-         <button className="text-red-700 uppercase">Edit</button>
-
+      {userListings && 
+      userListings.length > 0 && 
+      <div className="flex flex-col gap-4">
+       <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+       {userListings.map((listing) => (
+          <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <Link to={`/listing/${listing._id}`}>
+            <img src={listing.imageUrls[0]} alt="listing cover" 
+            className="h-16 w-16 object-contain"
+            />
+            </Link> 
+            <Link className="text-slate-700 semibold hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
+            <p>{listing.name}</p>
+            </Link>
+            
+            <div className="flex flex-col item-center">
+   
+           <button className="text-red-700 uppercase">Delete</button>
+           <button className="text-red-700 uppercase">Edit</button>
+  
+            </div>
+  
           </div>
-
-        </div>
-      ) }
+        ) )}
+      </div>}
     </div>
  
   );
